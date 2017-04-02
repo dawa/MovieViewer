c358dd8ebd61c18e0b9356bd3c253616ccb841e8
Davis Wamola <dawa009@yahoo.com>
2017-04-02 14:36:00 -0700

    Add custom navigation bar, no cell selection style

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -13,6 +13,8 @@ import MBProgressHUD
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var networkErrorView: UIView!
+    @IBOutlet weak var networkErrorImageView: UIImageView!
     
     var movies: [NSDictionary]?
     var api_key: String = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -23,6 +25,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
         tableView.delegate = self
         tableView.dataSource = self
+        networkErrorImageView.image = UIImage(named: "error")
+        networkErrorView.isHidden = true
 
         // Infinite Scrolling
         let tableFooterView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
@@ -32,48 +36,36 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableFooterView.addSubview(loadingView)
         self.tableView.tableFooterView = tableFooterView
         
+        // Pull down refresh control
         let refreshControl = UIRefreshControl()
         
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         // add refresh control to table view
         tableView.insertSubview(refreshControl, at: 0)
-        
-        let url = URL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(api_key)")
-        let request = URLRequest(url: url!)
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate:nil,
-            delegateQueue:OperationQueue.main
-        )
-        
-        // Display HUD right before the request is made
-        MBProgressHUD.showAdded(to: self.view, animated: true)
 
-        let task : URLSessionDataTask = session.dataTask(
-            with: request as URLRequest,
-            completionHandler: { (data, response, error) in
-                if let data = data {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(
-                        with: data, options:[]) as? NSDictionary {
-                        
-                        // Hide HUD once the network request comes back (must be done on main UI thread)
-                        MBProgressHUD.hide(for: self.view, animated: true)
-
-                        // This is where you will store the returned array of movies in your movies property
-                        self.movies = responseDictionary["results"] as? [NSDictionary]
-                        self.tableView.reloadData()
-                    }
-                }
-        });
-        task.resume()
+        // Customize Navigation Bar
+        self.navigationItem.title = "iFlicks"
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.tintColor = UIColor(red: 1.0, green: 0.25, blue: 0.25, alpha: 0.8)
+            
+            let shadow = NSShadow()
+            shadow.shadowColor = UIColor.blue.withAlphaComponent(0.5)
+            shadow.shadowOffset = CGSize(width: 2, height: 2)
+            shadow.shadowBlurRadius = 4;
+            navigationBar.titleTextAttributes = [
+                NSFontAttributeName : UIFont.boldSystemFont(ofSize: 22),
+                NSForegroundColorAttributeName : UIColor(red: 0.5, green: 0.15, blue: 0.15, alpha: 0.8),
+                NSShadowAttributeName : shadow
+            ]
+        }
+        
+        getMoviesData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
     
     // MARK: - Navigation
 
@@ -118,14 +110,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             }
         }
         
+        // No color when the user selects cell
+        cell.selectionStyle = .none
+
         return cell
     }    
 
-    func refreshControlAction(_ refreshControl: UIRefreshControl) {
-        
-        // ... Create the URLRequest `myRequest` ...
-        
-        // Configure session so that completion handler is executed on main UI thread
+    func getMoviesData() {
         let url = URL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(api_key)")
         let request = URLRequest(url: url!)
         let session = URLSession(
@@ -134,6 +125,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue:OperationQueue.main
         )
         
+        // Display HUD right before the request is made
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+
         let task : URLSessionDataTask = session.dataTask(
             with: request as URLRequest,
             completionHandler: { (data, response, error) in
@@ -141,14 +135,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     if let responseDictionary = try! JSONSerialization.jsonObject(
                         with: data, options:[]) as? NSDictionary {
                         
+                        // Hide HUD once the network request comes back (must be done on main UI thread)
+                        MBProgressHUD.hide(for: self.view, animated: true)
+
+                        // This is where you will store the returned array of movies in your movies property
                         self.movies = responseDictionary["results"] as? [NSDictionary]
                         self.tableView.reloadData()
-                        
-                        // Tell the refreshControl to stop spinning
-                        refreshControl.endRefreshing()
                     }
+                } else {
+                    self.networkErrorView.isHidden = false
                 }
         });
         task.resume()
+    }
+
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        getMoviesData()
+        // Tell the refreshControl to stop spinning
+        refreshControl.endRefreshing()
     }
 }
